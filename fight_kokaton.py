ab664f08c6614403969b5ad5db6f6aca7fafa021
hashiguchi <c0b22120f1@edu.teu.ac.jp>
2023-05-09 16:40:08 +0900

    練習4

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -7,7 +7,8 @@ import pygame as pg
 
 WIDTH = 1600  # ゲームウィンドウの幅
 HEIGHT = 900  # ゲームウィンドウの高さ
-NUM_OF_BOMBS=5
+NUM_OF_BOMBS = 5  # 爆弾の数
+
 
 def check_bound(area: pg.Rect, obj: pg.Rect) -> tuple[bool, bool]:
     """
@@ -41,7 +42,6 @@ class Bird:
         引数1 num：こうかとん画像ファイル名の番号
         引数2 xy：こうかとん画像の位置座標タプル
         """
-
         img0 = pg.transform.rotozoom(pg.image.load(f"ex03-20230509/fig/{num}.png"), 0, 2.0)  # 左向き，2倍
         img1 = pg.transform.flip(img0, True, False)  # 右向き，2倍
         self._imgs = {
@@ -54,7 +54,7 @@ class Bird:
             (0, +1): pg.transform.rotozoom(img1, -90, 1.0),  # 下
             (+1, +1): pg.transform.rotozoom(img1, -45, 1.0),  # 右下
         }
-        self._img = self._imgs[+1,0]
+        self._img = self._imgs[(+1, 0)]   # デフォルトで右      
         self._rct = self._img.get_rect()
         self._rct.center = xy
 
@@ -73,40 +73,39 @@ class Bird:
         引数1 key_lst：押下キーの真理値リスト
         引数2 screen：画面Surface
         """
-        sum_mv = [0,0]
+        sum_mv = [0, 0]
         for k, mv in __class__._delta.items():
             if key_lst[k]:
                 self._rct.move_ip(mv)
-                sum_mv[0] += mv[0]
-                sum_mv[1] += mv[1]
+                sum_mv[0] += mv[0]  # 横方向合計
+                sum_mv[1] += mv[1]  # 縦方向合計
         if check_bound(screen.get_rect(), self._rct) != (True, True):
             for k, mv in __class__._delta.items():
                 if key_lst[k]:
                     self._rct.move_ip(-mv[0], -mv[1])
-        if not sum_mv[0] == 0 and sum_mv[1] == 0:
-           self._img = self._imgs[tuple(sum_mv)] 
+        if not (sum_mv[0] == 0 and sum_mv[1] == 0):
+            self._img = self._imgs[tuple(sum_mv)]  # 押されたキーの合計値
         screen.blit(self._img, self._rct)
-        print(sum_mv)
+
+
 class Bomb:
     """
     爆弾に関するクラス
     """
-    _colors=[(255,0,0),(0,255,0),(0,0,255),(255,255,0),(255,0,255),(0,255,255)]
-    _dires=[-1,0,+1]
+    _colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255)]
+    _dires = [-1, 0, +1]
     def __init__(self):
         """
-        引数に基づき爆弾円Surfaceを生成する
-        引数1 color：爆弾円の色タプル
-        引数2 rad：爆弾円の半径
+        爆弾円Surfaceを生成する
         """
-        rad = random.randint(10,50)
+        rad = random.randint(10, 50)
         color = random.choice(Bomb._colors)
         self._img = pg.Surface((2*rad, 2*rad))
         pg.draw.circle(self._img, color, (rad, rad), rad)
         self._img.set_colorkey((0, 0, 0))
         self._rct = self._img.get_rect()
         self._rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
-        self._vx, self._vy = +1, +1
+        self._vx, self._vy = random.choice(Bomb._dires), random.choice(Bomb._dires)
 
     def update(self, screen: pg.Surface):
         """
@@ -121,26 +120,29 @@ class Bomb:
         self._rct.move_ip(self._vx, self._vy)
         screen.blit(self._img, self._rct)
 
+
 class Beam:
     """
-    beamに関するクラス
+    ビームに関するクラス
     """
-    def __init__(self, bird: int):
-        self._img = pg.transform.rotozoom( 
-                pg.image.load(f"ex03-20230509/fig/beam.png"), 
-                0, 
-                1.0)
-        self._rct = self._img.get_rect()
-        self._rct.centerx= bird._rct.centerx + bird._rct.width/2
-        self._rct.centery= bird._rct.centery
-        self._vx = 1
-        self._vy = 0
+    def __init__(self, bird: Bird):
+        """
+        割愛
+        """
+        self._img = pg.transform.rotozoom(pg.image.load(f"ex03-20230509/fig/beam.png"), 0, 2.0)  # 画像surface
+        self._rct = self._img.get_rect()  # 画像surfaceに対応したrect
+        self._rct.left = bird._rct.right  # こうかとんの右側にビームの左側を合わせる
+        self._rct.centery = bird._rct.centery
+        self._vx, self._vy = +1, 0
+
     def update(self, screen: pg.Surface):
         """
-        beamを速度ベクトルself._vx, self._vyに基づき移動させる
+        ビームを速度self._vyに基づき移動させる
+        引数 screen：画面Surface
         """
         self._rct.move_ip(self._vx, self._vy)
         screen.blit(self._img, self._rct)
+
 
 def main():
     pg.display.set_caption("たたかえ！こうかとん")
@@ -149,8 +151,9 @@ def main():
     bg_img = pg.image.load("ex03-20230509/fig/pg_bg.jpg")
 
     bird = Bird(3, (900, 400))
-    bombs = [Bomb() for i in range(NUM_OF_BOMBS)]
+    bombs = [Bomb() for _ in range(NUM_OF_BOMBS)]
     beam = None
+
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -158,8 +161,11 @@ def main():
                 return
             if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
                 beam = Beam(bird)
+
         tmr += 1
         screen.blit(bg_img, [0, 0])
+        
+        
         for bomb in bombs:
             bomb.update(screen)
             if bird._rct.colliderect(bomb._rct):
@@ -168,16 +174,19 @@ def main():
                 pg.display.update()
                 time.sleep(1)
                 return
+            
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
-        if beam is not None:
+
+        if beam is not None:  # ビームが存在しているとき
             beam.update(screen)
-            for i,bomb in enumerate(bombs):
+            for i, bomb in enumerate(bombs):
                 if beam._rct.colliderect(bomb._rct):
                     beam = None
                     del bombs[i]
                     bird.change_img(6, screen)
                     break
+
         pg.display.update()
         clock.tick(1000)
 
